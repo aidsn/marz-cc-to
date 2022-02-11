f39d7a684bf870bb123ea5b96380739cd4fbd9ba
tomgood18 <t0m@gmx.us>
2022-02-11 19:10:57 +1100

    Added Firebase Hosting

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -88,7 +88,7 @@ function App() {
       <Body>
         <Image src={logo} alt="react-logo" />
         <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
+          No Fucking Thanks
         </p>
         {/* Remove the "hidden" prop and open the JavaScript console in the browser to see what this function does */}
         <Button hidden onClick={() => readOnChainData()}>
